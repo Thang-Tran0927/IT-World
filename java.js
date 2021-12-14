4843ed8428e2f1e6db1ff8e7dac28ba13deef3dd
Thang-Tran0927 <95023209+Thang-Tran0927@users.noreply.github.com>
2021-12-14 08:56:55 +0700

    Add html css and java files

---
+++ java.js
@@ -0,0 +1,25 @@
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const teamMenu  = document.querySelector('#personal_info');
+    let scrollPos = window.scrollY;
+    console.log("it works")
+    if (scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        teamMenu.classList.remove('highlight');
+
+        return
+    } else if (scrollPos <1400) {
+        homeMenu.classList.remove('highlight');
+        teamMenu.classList.add('highlight');
+        
+        return
+    }
+
+    // if ((elem && scrollPos < 600) || elem) {
+    //     elem.classList.remove('highlight')
+    // }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
