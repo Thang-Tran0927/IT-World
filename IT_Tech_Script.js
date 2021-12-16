4e2f54b420a4add9b4118fe1ef1e637cbb951b7a
Bazinga <s3926080@rmit.edu.vn>
2021-12-16 10:23:32 +0700

    Bao Minh added IT_Tech.html, IT_Tech_Styles.css, IT_Tech_Script.js (for IT Technologies page)

---
+++ IT_Tech_Script.js
@@ -0,0 +1,7 @@
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const teamMenu  = document.querySelector('#personal_info');
+    const teamMenu.classList.add('highlight');
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
